8dfaca2bdd3b0cc03acf02b9eeddd88d394828c3
Parker Schless <pis7@cornell.edu>
2023-06-01 11:56:45 -0400

    Change Documentation and Fix Serial Read Bug

--- arduinoAnalogCollect/arduinoAnalogCollect.ino
+++ arduinoAnalogCollect/arduinoAnalogCollect.ino
@@ -2,19 +2,26 @@
 Title: Read ADC Values and Send to Serial
 Author: Parker Schless (pis7)
 Date: 6/1/23
-Version: 1.0
+Version: 1.1
+
+Summary:
+This is the Arduino support code for plotting sensor data in the Sensor Design course. This code initializes some variables including reference
+voltage used and delay time between samples which must match those in the additional Python code. Then, it simply reads in an analog voltage from
+a specified analog pin on the Arduino every delayTime milliseconds and converts it from the digital value back to a voltage to be sent over serial
+and used by the Python plotting code.
 
 References:
 - ADC: https://www.electronicwings.com/arduino/adc-in-arduino, https://cdn.arduino.cc/reference/en/language/functions/analog-io/analogreference/
 - Serial: https://www.arduino.cc/reference/en/language/functions/communication/serial/
 */
 
-float maxDigVal = 1023; // Maximum digital value of ADC (10-bit ADC)
+float maxDigVal = 1023; // Maximum digital value of ADC (10-bit ADC for the boards we are using)
 
 // Initialize variables - CHANGE FOLLOWING PARAMETERS AS NEEDED (MUST MATCH IN PYTHON) -----------------------------------------
 #define BAUDRATE 9600 // Serial baudrate
+#define ANALOGPIN 0 // Analog pin used for sensor input
+#define DELAYTIME 100 // Delay time in msec
 float vref = 5.0; // Reference voltage used
-int delayTime = 100; // Delay time in msec
 
 // Main functions, REMEMBER TO FLASH CODE TO ARDUINO IF CHANGES MADE -----------------------------------------------------------
 
@@ -24,8 +31,8 @@ void setup() {
 }
 
 void loop() {
-  float digitalVal = analogRead(0); // Read in digital value in range 0 - 1023 from ADC pin 0 (change number if using different pin)
+  float digitalVal = analogRead(ANALOGPIN); // Read in digital value in range 0 - 1023 from ADC pin
   float analogVoltage = vref*digitalVal/maxDigVal; // Convert digital value to analog voltage 
   Serial.println(analogVoltage); // Write analog voltage to serial
-  delay(delayTime); // delay for specified time before sending next sample
+  delay(DELAYTIME); // delay for specified time before sending next sample
 }
--- plotArduinoData.py
+++ plotArduinoData.py
@@ -2,7 +2,16 @@
 Title: Obtain annd Plot Arduino Data from Serial
 Author: Parker Schless (pis7)
 Date: 6/1/23
-Version: 1.0
+Version: 1.1
+
+Summary:
+Students will use this code in the Sensor Design course to obtain and plot the data (voltage output vs. time)
+from their sensors using an Arduino. This code features a 3 second countdown until data collection starts
+as well as the ability to change parmeters of the collection such as the delay time between samples for the Arduino,
+the total number of samples taken, and the reference voltage used for the Arduino's ADC. After the data
+is obtained, it can be plotted via Matplotlib's pyplot library and shown on the screen or saved to a
+PDF file. More importantly, the data is written to a CSV file so the students can analyze and plot the data on
+their own.
 
 References:
 - PySerial library: https://pyserial.readthedocs.io/en/latest/shortintro.html
@@ -17,24 +26,6 @@ import numpy as np
 import csv
 import time
 
-# Initialize variables - CHANGE THESE AS NEEDED ------------------------------------------------------------
-values = []
-count = 0 # Current number of samples obtained
-arduinoDelay = 100 # Delay time in msec used in Arduino code (MUST MATCH), use multiple of 100
-totalSamples = 301 # (Use multiple of 10 with an extra sample added), Total time = Arduino serial delay * totalSamples
-vref = 5.0 # Reference voltage used, keep at 5.0V unless explicityly stated otherwise
-
-# Initialize serial port, CHANGE COM PORT AS NEEDED (BAUDRATE MUST MATCH THAT IN ARDUINO ALSO)
-BAUDRATE = 9600 
-serialArduino = serial.Serial('COM11', BAUDRATE)
-
-# ----------------------------------------------------------------------------------------------------------
-
-# Handles closing serial port upon termination
-def exit_handler():
-    serialArduino.close()
-atexit.register(exit_handler)
-
 # Delay 3 seconds before starting data collection
 print('3')
 time.sleep(1)
@@ -42,10 +33,37 @@ print('2')
 time.sleep(1)
 print('1')
 time.sleep(1)
-print('Starting data collection')
+
+values = [] # Array to hold values of data points obtained
+count = 0 # Current number of samples obtained, keep it assigned to 0 here
+
+# Initialize variables - CHANGE THESE AS NEEDED ------------------------------------------------------------
+arduinoDelay = 100 # Delay time in msec used in Arduino code (MUST MATCH), use multiple of 100
+totalSamples = 101 # (Use multiple of 10 with an extra sample added), Total time = Arduino serial delay * totalSamples
+VREF = 5.0 # Reference voltage used, keep at 5.0V unless explicityly stated otherwise
+SENSORNAME = 'mysensor' # Name of sensor to be used for file names
+BAUDRATE = 9600 # Serial baud rate (MUST MATCH IN ARDUINO CODE)
+COMPORT = 'COM11' # Serial COM Port (SEARCH GOOGLE FOR HOW TO CHECK COM PORT NUMBER ON YOUR OS)
+
+# Note: If error saying permission denied to write to CSV file, close the CSV file before running this code again
+
+# ----------------------------------------------------------------------------------------------------------
+
+# Initialize serial connection
+serialArduino = serial.Serial(COMPORT, BAUDRATE)
+serialArduino.reset_input_buffer()
+
+# Handles closing serial port upon termination
+def exit_handler():
+    serialArduino.close()
+atexit.register(exit_handler)
 
 # Main loop
+print('Starting data collection')
 while True:
+    serialArduino.reset_input_buffer() # Flush input buffer to prevent invalid reads
+    while (serialArduino.in_waiting == 0): # Wait for serial buffer to have something in it
+        pass
     if count < totalSamples+1: # Obtain the specified number of samples from serial (+1 because we throw out the first junk sample at the end)
         valueRead = 0 # Initialize valueRead
         try: # Catch error if anomalous reading
@@ -53,15 +71,20 @@ while True:
         except: # If anomaly in serial data read, set this sample's value to the previous one to allow for data continuity
             if count != 0:
                 valueRead = values[-1]
+        if count != 0: # Only print out data points after first sample taken (one we throw out)
+            print('Sample: '+ str(count) + ', Voltage: ' + str(valueRead) + 'V')
+        if valueRead > VREF: # If invalid value on ADC, then exit
+            print('VALUE ERROR: PLEASE TRY AGAIN')
+            exit()
+        values.append(valueRead) # Append data point to values
         count += 1
-        values.append(valueRead) # Append to values
     else:
         values = values[1:] # Throw out first value in values - usually junk
+        endtime = time.time()
         break
 
-
 times = range(0, arduinoDelay*(len(values)), arduinoDelay) # Converts sample number to time in msec for plotting
-data = np.empty(shape=(2, len(values)))
+data = np.empty(shape=(2, len(values))) # Transpose 2D array of times and values so it is written properly to CSV file
 data[0] = times
 data[1] = values
 data = np.transpose(data)
@@ -69,18 +92,19 @@ data = np.transpose(data)
 print('Data collection completed')
 
 
-# Plot Data (Optional for quick viewing) CHANGE FOLLOWING PLOT PARAMETERS AS NEEDED ----------------------------------------------------------------
+# Plot Data (Optional for quick viewing) CHANGE FOLLOWING PLOT PARAMETERS AS NEEDED ----------------------------------------
 plt.plot(times, values)
-plt.ylim([0, vref]) # Sets y-axis range between 0V and reference voltage
-plt.xticks(range(0, arduinoDelay*(len(values)+1), arduinoDelay*10*(totalSamples // 100))) # Sets appropriate ticks for x-axis
+plt.ylim([0, VREF]) # Sets y-axis range between 0V and reference voltage
+plt.xticks(range(0, arduinoDelay*(len(values)+1), arduinoDelay*10*(totalSamples//100))) # Sets appropriate ticks for x-axis
 plt.xlabel('Time (msec)')
 plt.ylabel('Output Voltage (V)')
-plt.title('Output Voltage vs. Time for Sensor')
-#plt.show() # Show plot upon running
-plt.savefig('sensor.pdf') # Save plot to pdf file (CHANGE NAME AS NEEDED)
+plt.title('Output Voltage vs. Time for ' + SENSORNAME)
+#plt.show() # Show plot upon running (blocks creation of CSV file until closed, comment out if not needed)
+plt.savefig(SENSORNAME + ' Plot.pdf') # Save plot to pdf file
 
-# Write data to CSV file for analysis (CHANGE NAME AS NEEDED) --------------------------------------------------------------------------------------
-with open('sensor.csv', 'w', newline='') as f:
+# Write data to CSV file for analysis --------------------------------------------------------------------------------------
+with open(SENSORNAME + ' Data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
+    writer.writerow(['Time (msec)', 'Voltage (V)'])
     for row in data:
         writer.writerow(row)
