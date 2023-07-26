969cd3f00a2d0251b9f501080b30f9ee28001262
Parker Schless <pis7@cornell.edu>
2023-07-26 12:31:59 -0400

    Add v1.0 of Speedometer code

--- speedometer/speedometer.ino
+++ speedometer/speedometer.ino
@@ -1,14 +1,10 @@
 /*
-Title: Read ADC Values and Send to Serial
+Title: Calculate Linear Speed of Wheel
 Author: Parker Schless (pis7)
-Date: 6/1/23
-Version: 1.1
+Date: 7/26/23
+Version: 1.0
 
 Summary:
-This is the Arduino support code for plotting sensor data in the Sensor Design course. This code initializes some variables including reference
-voltage used and delay time between samples which must match those in the additional Python code. Then, it simply reads in an analog voltage from
-a specified analog pin on the Arduino every delayTime milliseconds and converts it from the digital value back to a voltage to be sent over serial
-and used by the Python plotting code.
 
 References:
 - ADC: https://www.electronicwings.com/arduino/adc-in-arduino, https://cdn.arduino.cc/reference/en/language/functions/analog-io/analogreference/
@@ -21,7 +17,12 @@ float maxDigVal = 1023; // Maximum digital value of ADC (10-bit ADC for the boar
 #define BAUDRATE 9600 // Serial baudrate
 #define ANALOGPIN 0 // Analog pin used for sensor input
 #define DELAYTIME 1 // Delay time in msec
+#define CIRCUM 2*3.14159265*0.5625 // 2*pi*radius (ft)
 float vref = 5.0; // Reference voltage used
+int prevTime = 0;
+int thisTime = 0;
+bool getPtDone = false;
+
 
 // Main functions, REMEMBER TO FLASH CODE TO ARDUINO IF CHANGES MADE -----------------------------------------------------------
 
@@ -33,14 +34,16 @@ void setup() {
 void loop() {
   float digitalVal = analogRead(ANALOGPIN); // Read in digital value in range 0 - 1023 from ADC pin
   float analogVoltage = vref*digitalVal/maxDigVal; // Convert digital value to analog voltage
-
-  // Comment below out if don't want bounds on serial plotter (if using this code to take actual data then having the bounds will introduce unwanted values)
-  Serial.print(0);
-  Serial.print(" ");
-  Serial.print(5);
-  Serial.print(" ");
-  // ---
-  
-  Serial.println(analogVoltage); // Write analog voltage to serial
+  if (analogVoltage > 1.0 && getPtDone == false) {
+    prevTime = thisTime;
+    thisTime = millis(); // Obtain time points
+    float speed = CIRCUM / (float) (thisTime - prevTime); // v = d/t in ft/ms
+    speed = 1000.0 * 3600.0 * speed / 5280.0; // get v in mph
+    Serial.println(speed);
+    getPtDone = true;
+  }
+  if (analogVoltage < 1.0 && getPtDone == true) { // Once analogVoltage has decreased back down from a high level, reset bool to trigger on next point
+    getPtDone = false;
+  }
   delay(DELAYTIME); // delay for specified time before sending next sample
 }
