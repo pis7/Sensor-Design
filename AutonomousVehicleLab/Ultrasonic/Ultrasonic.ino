83fb5cb3fc0ab0a730a3603f2d516fca60156c52
Parker Schless <pis7@cornell.edu>
2023-08-14 19:12:19 -0400

    Add 3D printed parts

--- AutonomousVehicleLab/Ultrasonic/Ultrasonic.ino
+++ AutonomousVehicleLab/Ultrasonic/Ultrasonic.ino
@@ -3,11 +3,12 @@
 * http://www.kerrywong.com
 */
 
-#define FREQ 40000.0 // Hz
+#define FREQ 40000.0 // Frequency in Hz of transmitter and receiver used
 #define DUTYCYCLE 0.5 // 0 - 1 (0% - 100%)
 
-// NOTE: THIS CODE IS CALIBRATED FOR RANGES UP TO 20 FT
+// NOTE: THIS CODE IS CALIBRATED FOR RANGES UP TO 20 FT with RECVWINDOW = 356
 #define RECVWINDOW 256 // 256 (empirically) corresponds to about 20 ms to search for peak value (can change this depending on desired range), assume peak comes from 1st echo
+#define TRANSMITTERFADE 2 // Time in milliseconds for initial transmitter signal to fade as it is picked up when generated, shorten this to get readings at smaller distances, 1 ms ~= shortest measureable distance is 1 ft
 
 byte a = 0; // ADC value of current reading
 unsigned long t_start = 0; // Start time after transducer sends signal
@@ -20,24 +21,22 @@ float d = 0; // Distance in meters
 void setup()
 {
   Serial.begin(9600);
-
   pinMode(12, OUTPUT);
 }
 
 void loop()
 {  
-  // Generate PWM pulse on transducer
+  // Generate PWM pulse on transducer for 300 us
   startTransducer(FREQ, DUTYCYCLE);
   delayMicroseconds(300);
   stopTransducer();
-  //delay(1000);
 
   v_peak = 0;
-  t_start = micros(); // Get start time
+  t_start = micros(); // Get start time (time when receiver signal stops)
   t_peak = t_start;
-  delayMilliseconds(1); // Wait for initial receiver signal to fade
+  delayMilliseconds(TRANSMITTERFADE); // Wait for initial transmitter signal to fade
 
-  
+  // Get time of peak in first echo
   for (int i = 0; i < RECVWINDOW; i++) {
     a = analogRead(0); // Read receiver analog pin
     t = micros();
@@ -48,9 +47,14 @@ void loop()
     }
   }
 
-  t = t_peak - t_start; // Calculate time difference based on initial time and time of greates peak in interval
-  d = (float) t * SPEED_OF_SOUND_20C / 2.0; // dist = time * c / 2
-  Serial.println(d , 2); // Print distance to two decimal places in meters
+  t = t_peak - t_start; // Calculate time difference based on initial time and time of greatest peak in interval
+  d = (float) t * SPEED_OF_SOUND_20C / 2.0; // dist = time * c / 2 in cm
+  int d_int = (int) (d*100);
+
+  // Format output
+  char output[128];
+  sprintf(output, "Distance: %d cm", d_int); // Arduino does not support sprintf with floats or decimals, convert to int therefore
+  Serial.println(output); // Print distance in cm
 }
 
 void delayMilliseconds(int ms) {
@@ -76,10 +80,10 @@ void startTransducer(float freq, float dutyCycle)
   TCCR1B = _BV(WGM13) | _BV(CS10) | _BV(ICNC1); // Set Timer/Counter1 Control Register B with 10010001 for PWM
   //f0 = fclk / (2 * N * Top)
   long topv = (long) ((float) F_CPU /(freq * 2.0 * 1.0)); 
-  ICR1 = topv; // Set Input Capcture Register with topv
+  ICR1 = topv; // Set Input Capcture Register 1 with topv
 
   OCR1B = (int) ((float) topv * (dutyCycle)); // Set Ouput Capture Register lB
-  DDRB |= _BV(PORTB6); // Set Port B data direction register pin B6 to high (output)
+  DDRB |= _BV(PORTB6); // Set Port B data direction register pin B6 (digital pin 12) to high (output)
   TCCR1A = _BV(COM1A1) | _BV(COM1B1); // Set Timer/Counter1 Control Register A with 10100000
   sei(); // Enable interrupts
 }
