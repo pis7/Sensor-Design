1f4478c16d56768d2774fe6d7af58abe43742982
Parker Schless <pis7@cornell.edu>
2023-09-07 11:46:20 -0400

    Gas lab 3D printed beaker holder changed

--- GasLab/CO2Sensor/CO2Sensor.ino
+++ GasLab/CO2Sensor/CO2Sensor.ino
@@ -22,36 +22,59 @@ void setup() {
 }
 
 void loop() {
-
   // Change opmode to standard gas sensing mode (starts up in idle mode from POR)
-  Wire.begin(ADDR);
+  Wire.beginTransmission(ADDR);
   Wire.write(byte(0x10)); // Address of OPMODE register
   Wire.write(byte(0x02)); // OPMODE register
   Wire.endTransmission();
 
   // Read status register until new data ready
   while (new_data == false) {
-    Wire.begin(ADDR);
+    Wire.beginTransmission(ADDR);
     Wire.write(byte(0x20)); // DEVICE_STATUS register
     Wire.endTransmission();
-    delay(70);
     Wire.requestFrom(ADDR, 1); // Read 1 byte from status register
     if (Wire.available() == 1) {
-      if ((Wire.read() & 0x02) != 0) { // New data avilable if bit 1 (NEWDAT) is high
+      uint8_t read = Wire.read();
+      if ((read & 0x02) != 0) { // New data avilable if bit 1 (NEWDAT) is high
         new_data = true;
       }
     }
   }
   new_data = false;
 
+  uint8_t upperByte = 0;
+  uint8_t lowerByte = 0;
+
   // Read eCO2
-  Wire.begin(ADDR);
+  Wire.beginTransmission(ADDR);
   Wire.write(byte(0x24)); // DATA_ECO2 register
-  Wire.endTransmission();
-  delay(70);
+  Wire.endTransmission(false);
   Wire.requestFrom(ADDR, 2); // Read 2 bytes from eCO2 register
-  uint16_t reading = (Wire.read() << 8) | Wire.read(); // Shift MSB to left 8 bits and bitwise or with LSB to get full 16-bit eCO2 value
+  if (Wire.available() == 2) {
+    lowerByte = Wire.read();
+    upperByte = Wire.read();
+  } else {
+    lowerByte = 0;
+    upperByte = 0;
+  }
+  uint16_t eco2 = (upperByte << 8) | lowerByte; // Shift MSB to left 8 bits and bitwise or with LSB to get full 16-bit eCO2 value
+  
+  // Read TVOC
+  Wire.beginTransmission(ADDR);
+  Wire.write(byte(0x22)); // DATA_ECO2 register
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADDR, 2); // Read 2 bytes from eCO2 register
+  if (Wire.available() == 2) {
+    lowerByte = Wire.read();
+    upperByte = Wire.read();
+  } else {
+    lowerByte = 0;
+    upperByte = 0;
+  }
+  uint16_t tvoc = (upperByte << 8) | lowerByte; // Shift MSB to left 8 bits and bitwise or with LSB to get full 16-bit eCO2 value
+  
   char output[256];
-  sprintf(output, "eCO2: %u ppm", reading); // Format output string
-  Serial.println("eCO2");
+  sprintf(output, "eCO2: %u ppm, TVOC: %u ppb", eco2, tvoc); // Format output string
+  Serial.println(output);
 }
